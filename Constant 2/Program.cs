734afbb76de811742746208a3197e4e599946ea2
jerit6490 <jerit6490@2Q3H0Q2>
2022-09-15 13:30:07 -0400

    Add project files.

---
+++ Constant 2/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    enum Days { Sat, Sun, Mon, Tue, Wed, Thur, Fri };
+    private static void Main(string[] args)
+    {
+        const double pi = 3.14159265358979;
+        var daysType = typeof(Days);
+        Console.WriteLine("Pi Type: " +pi.GetType());
+        Console.WriteLine("Circumference: " + (3 * pi));
+        Console.WriteLine("\nFirst Name: " + Days.Sat);
+        Console.WriteLine("1st Index: " + (int)Days.Sat);
+        string name = Enum.GetName(daysType, 1);
+        Console.WriteLine("\n2nd Index: " + name);
+        bool flag = Enum.IsDefined(daysType, "Mon");
+        Console.WriteLine("Contains Mon?: " + flag);
+        Console.ReadKey();
+
+
+    }
+}
